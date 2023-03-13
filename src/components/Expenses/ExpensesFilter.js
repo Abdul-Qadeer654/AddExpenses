fc5a026fb8c59766fc5297d8c4550dd5651c8bc9
Abdul Qadeer <kadeercs@gmail.com>
2023-03-13 21:52:10 +0100

    add a apce

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -5,9 +5,10 @@ import './ExpensesFilter.css';
 const ExpensesFilter = (props) => {
   const dropdownChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
-  };
+  }; 
 
   return (
+    
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
